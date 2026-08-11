3b81cbb3c83c44865e0d560b6143adf82964c596
Ash Williams <246395973+ashwilliams7-code@users.noreply.github.com>
2026-08-11 20:49:14 +1000

    Launch SKY Intelligence ecosystem and founder profile

--- assets/site.js
+++ assets/site.js
@@ -330,7 +330,7 @@
         '<a href="/">Home</a>'+
         '<a href="/#method">Method</a>'+
         '<a href="/#solutions">Solutions</a>'+
-        '<a class="nav-sky-intelligence" href="/executive-intelligence.html"'+(path.endsWith('/executive-intelligence.html')?' aria-current="page"':'')+'>Sky</a>'+
+        '<a class="nav-sky-intelligence" href="/executive-intelligence.html"'+(path.endsWith('/executive-intelligence.html')?' aria-current="page"':'')+'>SKY Intelligence</a>'+
         '<a href="/#engagements">Engagements</a>'+
         '<a href="/#faq">FAQ</a>'+
         '<a href="/founder.html"'+(path.endsWith('/founder.html')?' aria-current="page"':'')+'>Founder</a>';
